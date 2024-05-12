541d6be70d03145eb581084ec3769acb2d4156dd
eduvedras <edu.vedras@gmail.com>
2024-05-12 11:16:22 +0100

    Day1 completed

--- 2023/Day1/ex1.js
+++ 2023/Day1/ex1.js
@@ -1,4 +1,14 @@
 var fs = require("fs");
+var digit = new Map();
+digit.set("one", 1);
+digit.set("two", 2);
+digit.set("three", 3);
+digit.set("four", 4);
+digit.set("five", 5);
+digit.set("six", 6);
+digit.set("seven", 7);
+digit.set("eight", 8);
+digit.set("nine", 9);
 function isNumber(val) {
     return val != null && val !== "" && !isNaN(Number(val.toString()));
 }
@@ -19,10 +29,36 @@ function getvals(data) {
                 value = value * Number(el[i]);
                 break;
             }
+            var substring = el.slice(0, i + 1);
+            var gotfirstnumber = false;
+            for (var _i = 0, _a = Array.from(digit.keys()); _i < _a.length; _i++) {
+                var key = _a[_i];
+                if (substring.includes(key)) {
+                    value = value * digit.get(key);
+                    gotfirstnumber = true;
+                    break;
+                }
+            }
+            if (gotfirstnumber) {
+                break;
+            }
         }
         for (var j = el.length - 1; j >= 0; j--) {
             if (isNumber(el[j])) {
                 value = value + Number(el[j]);
+                break;
+            }
+            var substring = el.slice(j, el.length);
+            var gotsecondnumber = false;
+            for (var _b = 0, _c = Array.from(digit.keys()); _b < _c.length; _b++) {
+                var key = _c[_b];
+                if (substring.includes(key)) {
+                    value = value + digit.get(key);
+                    gotsecondnumber = true;
+                    break;
+                }
+            }
+            if (gotsecondnumber) {
                 break;
             }
         }
--- 2023/Day1/ex1.ts
+++ 2023/Day1/ex1.ts
@@ -1,5 +1,17 @@
 const fs = require("fs");
 
+const digit = new Map<string, number>();
+
+digit.set("one", 1);
+digit.set("two", 2);
+digit.set("three", 3);
+digit.set("four", 4);
+digit.set("five", 5);
+digit.set("six", 6);
+digit.set("seven", 7);
+digit.set("eight", 8);
+digit.set("nine", 9);
+
 function isNumber(val: string | number) {
 	return val != null && val !== "" && !isNaN(Number(val.toString()));
 }
@@ -18,9 +30,24 @@ function getvals(data: string): Array<number> {
 
 	list.forEach((el) => {
 		let value: number = 10;
+
 		for (let i = 0; i < el.length; i++) {
 			if (isNumber(el[i])) {
 				value = value * Number(el[i]);
+				break;
+			}
+			const substring: string = el.slice(0, i + 1);
+
+			let gotfirstnumber: boolean = false;
+			for (let key of Array.from(digit.keys())) {
+				if (substring.includes(key)) {
+					value = value * digit.get(key)!;
+					gotfirstnumber = true;
+					break;
+				}
+			}
+
+			if (gotfirstnumber) {
 				break;
 			}
 		}
@@ -28,6 +55,21 @@ function getvals(data: string): Array<number> {
 		for (let j = el.length - 1; j >= 0; j--) {
 			if (isNumber(el[j])) {
 				value = value + Number(el[j]);
+				break;
+			}
+
+			const substring: string = el.slice(j, el.length);
+
+			let gotsecondnumber: boolean = false;
+			for (let key of Array.from(digit.keys())) {
+				if (substring.includes(key)) {
+					value = value + digit.get(key)!;
+					gotsecondnumber = true;
+					break;
+				}
+			}
+
+			if (gotsecondnumber) {
 				break;
 			}
 		}
