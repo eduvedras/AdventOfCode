c8ce0dad9b8f64856de7982949fddec62ab51b66
eduvedras <edu.vedras@gmail.com>
2024-05-13 15:53:35 +0100

    Day 3 part 2

--- 2023/Day3/ex3.js
+++ 2023/Day3/ex3.js
@@ -2,110 +2,135 @@ var fs = require("fs");
 function isNumeric(value) {
     return /\d/.test(value);
 }
-function isSpecialCharacter(value) {
+function isSpecialCharacter(value, i, j, gears) {
+    var gear = { x: i, y: j };
+    if (value === "*" &&
+        !gears.some(function (gear) { return gear.x === gear.x && gear.y === gear.y; })) {
+        gears.push(gear);
+    }
     return value !== "." && !isNumeric(value);
 }
 try {
     var data = fs.readFileSync("input.txt", "utf8");
     var lines = data.split("\n");
-    var result = 0;
+    var numbers = new Map();
+    var result_1 = 0;
     for (var i = 0; i < lines.length; i++) {
         var number = "";
         var is_part_number = false;
+        var gears = [];
         for (var j = 0; j < lines[i].length; j++) {
             if (isNumeric(lines[i][j])) {
                 number += lines[i][j];
                 if (i === 0 && j === 0) {
-                    if (isSpecialCharacter(lines[i][j + 1]) ||
-                        isSpecialCharacter(lines[i + 1][j]) ||
-                        isSpecialCharacter(lines[i + 1][j + 1])) {
+                    if (isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+                        isSpecialCharacter(lines[i + 1][j + 1], i + 1, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (i === 0 && j === lines[i].length - 1) {
-                    if (isSpecialCharacter(lines[i][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j])) {
+                    if (isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j - 1], i + 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (i === lines.length - 1 && j === 0) {
-                    if (isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i - 1][j + 1]) ||
-                        isSpecialCharacter(lines[i][j + 1])) {
+                    if (isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i - 1][j + 1], i - 1, j + 1, gears) ||
+                        isSpecialCharacter(lines[i][j + 1], i, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (i === lines.length - 1 &&
                     j === lines[i].length - 1) {
-                    if (isSpecialCharacter(lines[i - 1][j - 1]) ||
-                        isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i][j - 1])) {
+                    if (isSpecialCharacter(lines[i - 1][j - 1], i - 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i][j - 1], i, j - 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (i === 0) {
-                    if (isSpecialCharacter(lines[i][j - 1]) ||
-                        isSpecialCharacter(lines[i][j + 1]) ||
-                        isSpecialCharacter(lines[i + 1][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j]) ||
-                        isSpecialCharacter(lines[i + 1][j + 1])) {
+                    if (isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+                        isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j - 1], i + 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+                        isSpecialCharacter(lines[i + 1][j + 1], i + 1, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (i === lines.length - 1) {
-                    if (isSpecialCharacter(lines[i - 1][j - 1]) ||
-                        isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i - 1][j + 1]) ||
-                        isSpecialCharacter(lines[i][j - 1]) ||
-                        isSpecialCharacter(lines[i][j + 1])) {
+                    if (isSpecialCharacter(lines[i - 1][j - 1], i - 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i - 1][j + 1], i - 1, j + 1, gears) ||
+                        isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+                        isSpecialCharacter(lines[i][j + 1], i, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (j === 0) {
-                    if (isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i - 1][j + 1]) ||
-                        isSpecialCharacter(lines[i][j + 1]) ||
-                        isSpecialCharacter(lines[i + 1][j]) ||
-                        isSpecialCharacter(lines[i + 1][j + 1])) {
+                    if (isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i - 1][j + 1], i - 1, j + 1, gears) ||
+                        isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+                        isSpecialCharacter(lines[i + 1][j + 1], i + 1, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
                 else if (j === lines[i].length - 1) {
-                    if (isSpecialCharacter(lines[i - 1][j - 1]) ||
-                        isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j])) {
+                    if (isSpecialCharacter(lines[i - 1][j - 1], i - 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j - 1], i + 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears)) {
                         is_part_number = true;
                     }
                 }
                 else {
-                    if (isSpecialCharacter(lines[i - 1][j - 1]) ||
-                        isSpecialCharacter(lines[i - 1][j]) ||
-                        isSpecialCharacter(lines[i - 1][j + 1]) ||
-                        isSpecialCharacter(lines[i][j - 1]) ||
-                        isSpecialCharacter(lines[i][j + 1]) ||
-                        isSpecialCharacter(lines[i + 1][j - 1]) ||
-                        isSpecialCharacter(lines[i + 1][j]) ||
-                        isSpecialCharacter(lines[i + 1][j + 1])) {
+                    if (isSpecialCharacter(lines[i - 1][j - 1], i - 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+                        isSpecialCharacter(lines[i - 1][j + 1], i - 1, j + 1, gears) ||
+                        isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+                        isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j - 1], i + 1, j - 1, gears) ||
+                        isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+                        isSpecialCharacter(lines[i + 1][j + 1], i + 1, j + 1, gears)) {
                         is_part_number = true;
                     }
                 }
             }
             else {
                 if (is_part_number) {
-                    result += Number(number);
+                    numbers.set(i + "," + j + "," + number, gears);
                 }
                 number = "";
                 is_part_number = false;
+                gears = [];
             }
             if (j === lines[i].length - 1 && is_part_number) {
-                result += Number(number);
+                numbers.set(i + "," + j + "," + number, gears);
             }
         }
     }
-    console.log(result);
+    var gearskeys_1 = new Map();
+    numbers.forEach(function (gears, number) {
+        gears.forEach(function (gear) {
+            var key = gear.x.toString() + "," + gear.y.toString();
+            if (gearskeys_1.has(key)) {
+                gearskeys_1.get(key).push(Number(number.split(",")[2]));
+            }
+            else {
+                gearskeys_1.set(key, [Number(number.split(",")[2])]);
+            }
+        });
+    });
+    gearskeys_1.forEach(function (numbers, gear) {
+        if (numbers.length === 2) {
+            result_1 += numbers[0] * numbers[1];
+        }
+    });
+    console.log(result_1);
 }
 catch (err) {
     console.error(err);
--- 2023/Day3/ex3.ts
+++ 2023/Day3/ex3.ts
@@ -4,44 +4,79 @@ function isNumeric(value: string): boolean {
 	return /\d/.test(value);
 }
 
-function isSpecialCharacter(value: string): boolean {
+function isSpecialCharacter(
+	value: string,
+	i: number,
+	j: number,
+	gears: Array<gearpos>
+): boolean {
+	const gear: gearpos = { x: i, y: j };
+
+	if (
+		value === "*" &&
+		!gears.some((gear) => gear.x === gear.x && gear.y === gear.y)
+	) {
+		gears.push(gear);
+	}
 	return value !== "." && !isNumeric(value);
+}
+
+interface gearpos {
+	x: number;
+	y: number;
 }
 
 try {
 	const data: string = fs.readFileSync("input.txt", "utf8");
 	const lines: Array<string> = data.split("\n");
+	const numbers = new Map<string, Array<gearpos>>();
 
 	let result: number = 0;
 
 	for (let i = 0; i < lines.length; i++) {
 		let number: string = "";
 		let is_part_number: boolean = false;
+		let gears: Array<gearpos> = [];
 		for (let j = 0; j < lines[i].length; j++) {
 			if (isNumeric(lines[i][j])) {
 				number += lines[i][j];
 
 				if (i === 0 && j === 0) {
 					if (
-						isSpecialCharacter(lines[i][j + 1]) ||
-						isSpecialCharacter(lines[i + 1][j]) ||
-						isSpecialCharacter(lines[i + 1][j + 1])
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j + 1],
+							i + 1,
+							j + 1,
+							gears
+						)
 					) {
 						is_part_number = true;
 					}
 				} else if (i === 0 && j === lines[i].length - 1) {
 					if (
-						isSpecialCharacter(lines[i][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j])
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j - 1],
+							i + 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears)
 					) {
 						is_part_number = true;
 					}
 				} else if (i === lines.length - 1 && j === 0) {
 					if (
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i - 1][j + 1]) ||
-						isSpecialCharacter(lines[i][j + 1])
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i - 1][j + 1],
+							i - 1,
+							j + 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears)
 					) {
 						is_part_number = true;
 					}
@@ -50,79 +85,165 @@ try {
 					j === lines[i].length - 1
 				) {
 					if (
-						isSpecialCharacter(lines[i - 1][j - 1]) ||
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i][j - 1])
+						isSpecialCharacter(
+							lines[i - 1][j - 1],
+							i - 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears)
 					) {
 						is_part_number = true;
 					}
 				} else if (i === 0) {
 					if (
-						isSpecialCharacter(lines[i][j - 1]) ||
-						isSpecialCharacter(lines[i][j + 1]) ||
-						isSpecialCharacter(lines[i + 1][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j]) ||
-						isSpecialCharacter(lines[i + 1][j + 1])
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j - 1],
+							i + 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j + 1],
+							i + 1,
+							j + 1,
+							gears
+						)
 					) {
 						is_part_number = true;
 					}
 				} else if (i === lines.length - 1) {
 					if (
-						isSpecialCharacter(lines[i - 1][j - 1]) ||
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i - 1][j + 1]) ||
-						isSpecialCharacter(lines[i][j - 1]) ||
-						isSpecialCharacter(lines[i][j + 1])
+						isSpecialCharacter(
+							lines[i - 1][j - 1],
+							i - 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i - 1][j + 1],
+							i - 1,
+							j + 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears)
 					) {
 						is_part_number = true;
 					}
 				} else if (j === 0) {
 					if (
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i - 1][j + 1]) ||
-						isSpecialCharacter(lines[i][j + 1]) ||
-						isSpecialCharacter(lines[i + 1][j]) ||
-						isSpecialCharacter(lines[i + 1][j + 1])
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i - 1][j + 1],
+							i - 1,
+							j + 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j + 1],
+							i + 1,
+							j + 1,
+							gears
+						)
 					) {
 						is_part_number = true;
 					}
 				} else if (j === lines[i].length - 1) {
 					if (
-						isSpecialCharacter(lines[i - 1][j - 1]) ||
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j])
+						isSpecialCharacter(
+							lines[i - 1][j - 1],
+							i - 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j - 1],
+							i + 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears)
 					) {
 						is_part_number = true;
 					}
 				} else {
 					if (
-						isSpecialCharacter(lines[i - 1][j - 1]) ||
-						isSpecialCharacter(lines[i - 1][j]) ||
-						isSpecialCharacter(lines[i - 1][j + 1]) ||
-						isSpecialCharacter(lines[i][j - 1]) ||
-						isSpecialCharacter(lines[i][j + 1]) ||
-						isSpecialCharacter(lines[i + 1][j - 1]) ||
-						isSpecialCharacter(lines[i + 1][j]) ||
-						isSpecialCharacter(lines[i + 1][j + 1])
+						isSpecialCharacter(
+							lines[i - 1][j - 1],
+							i - 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i - 1][j], i - 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i - 1][j + 1],
+							i - 1,
+							j + 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i][j - 1], i, j - 1, gears) ||
+						isSpecialCharacter(lines[i][j + 1], i, j + 1, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j - 1],
+							i + 1,
+							j - 1,
+							gears
+						) ||
+						isSpecialCharacter(lines[i + 1][j], i + 1, j, gears) ||
+						isSpecialCharacter(
+							lines[i + 1][j + 1],
+							i + 1,
+							j + 1,
+							gears
+						)
 					) {
 						is_part_number = true;
 					}
 				}
 			} else {
 				if (is_part_number) {
-					result += Number(number);
+					numbers.set(i + "," + j + "," + number, gears);
 				}
 				number = "";
 				is_part_number = false;
+				gears = [];
 			}
 
 			if (j === lines[i].length - 1 && is_part_number) {
-				result += Number(number);
+				numbers.set(i + "," + j + "," + number, gears);
 			}
 		}
 	}
+
+	const gearskeys = new Map<string, Array<number>>();
+
+	numbers.forEach((gears: Array<gearpos>, number: string) => {
+		gears.forEach((gear: gearpos) => {
+			let key: string = gear.x.toString() + "," + gear.y.toString();
+			if (gearskeys.has(key)) {
+				gearskeys.get(key)!.push(Number(number.split(",")[2]));
+			} else {
+				gearskeys.set(key, [Number(number.split(",")[2])]);
+			}
+		});
+	});
+
+	gearskeys.forEach((numbers: Array<number>, gear: string) => {
+		if (numbers.length === 2) {
+			result += numbers[0] * numbers[1];
+		}
+	});
+
 	console.log(result);
 } catch (err) {
 	console.error(err);
